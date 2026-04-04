27c5dd452d5b7d9af40ae69989dbf592f9fe690a
Ansory <45823062+Ansory@users.noreply.github.com>
2026-04-04 23:54:16 +0700

    Update and rename vite.config.js to Vite.config.mjs

--- Vite.config.mjs
+++ Vite.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
   plugins: [react()],
-  // root adalah folder tempat index.html berada (default: project root)
   root: '.',
   base: './',
   build: {
@@ -13,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src/renderer'),
+      '@': fileURLToPath(new URL('./src/renderer', import.meta.url)),
     },
   },
   server: {

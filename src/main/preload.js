54fdde6eb8e0d831dbe52dba6452e45172be3d91
Ansory <45823062+Ansory@users.noreply.github.com>
2026-04-05 13:58:59 +0700

    Update preload.js

--- src/main/preload.js
+++ src/main/preload.js
@@ -154,11 +154,23 @@ contextBridge.exposeInMainWorld('electron', {
     facebookUploadVideo: (options) => ipcRenderer.invoke('facebook:upload-video', options),
     facebookGetInsights: (options) => ipcRenderer.invoke('facebook:get-insights', options),
 
-    // ============ MODUL 33: AUTO UPDATE ============
+    // ============ MODUL 33: AUTO UPDATE - LENGKAP ============
     getConfig: () => ipcRenderer.invoke('updater:get-config'),
     updateConfig: (config) => ipcRenderer.invoke('updater:update-config', config),
     getVersion: () => ipcRenderer.invoke('updater:get-version'),
     checkForUpdates: () => ipcRenderer.invoke('updater:check'),
+    
+    // Untuk UpdateNotification.jsx
+    getSkippedVersions: () => ipcRenderer.invoke('updater:get-skipped-versions'),
+    skipUpdate: (version) => ipcRenderer.invoke('updater:skip-update', version),
+    downloadUpdate: () => ipcRenderer.invoke('updater:download-update'),
+    installUpdate: () => ipcRenderer.invoke('updater:install-update'),
+    
+    // Event listeners untuk update
+    onUpdateAvailable: (callback) => ipcRenderer.on('updater:update-available', (event, ...args) => callback(event, ...args)),
+    onUpdateDownloadProgress: (callback) => ipcRenderer.on('updater:download-progress', (event, ...args) => callback(event, ...args)),
+    onUpdateDownloaded: (callback) => ipcRenderer.on('updater:update-downloaded', (event, ...args) => callback(event, ...args)),
+    onUpdateError: (callback) => ipcRenderer.on('updater:error', (event, ...args) => callback(event, ...args)),
 
     // ============ MODUL 35: CONTENT REPURPOSING ============
     repurposeResize: (options) => ipcRenderer.invoke('repurpose:resize', options),
@@ -201,13 +213,12 @@ contextBridge.exposeInMainWorld('electron', {
     getStore: (key) => ipcRenderer.invoke('store:get', key),
     setStore: (key, value) => ipcRenderer.invoke('store:set', key, value),
 
-    // ============ EVENT LISTENERS ============
-    onUpdateStatus: (callback) => ipcRenderer.on('update-status', (event, ...args) => callback(...args)),
-
+    // ============ EVENT LISTENERS GENERIC ============
     on: (channel, callback) => {
         const validChannels = [
-            'update-status', 'update-available', 'update-downloaded', 'update-error',
-            'update-download-progress', 'viral-alert', 'copyright-strike', 'upload-complete'
+            'update-status', 'updater:update-available', 'updater:download-progress', 
+            'updater:update-downloaded', 'updater:error',
+            'viral-alert', 'copyright-strike', 'upload-complete'
         ];
         if (validChannels.includes(channel)) {
             ipcRenderer.on(channel, (event, ...args) => callback(...args));
